8b70d958bc2490de0c3e1c48831df3ba23f7ec34
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-03-18 00:21:25 +0900

    C# 프로그래밍 1주차 수업

---
+++ C_sharp_solution/C_sharp_project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_sharp_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("세종대 유정란");
+            Console.WriteLine("세종대 유정란");
+        }
+    }
+}
