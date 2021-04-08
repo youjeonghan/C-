1e24bd63e0d255518aedc836738b67fd96c20582
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-09 00:13:13 +0900

    4주차-1

---
+++ Chapter3/MultipleClass2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultipleClass2
+{
+    class Test
+    {
+        string name;
+        public void input()
+        {
+            name = "University";
+            Console.WriteLine(name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test obj = new Test();
+            obj.input();
+        }
+    }
+}
