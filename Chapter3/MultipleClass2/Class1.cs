34e3f1427e607b964cc31a7192cd79efdef5e3c5
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-09 00:25:51 +0900

    4주차-2

---
+++ Chapter3/MultipleClass2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleClass2
+{
+    class Class1
+    {
+        string name;
+        int id;
+        public void input()
+        {
+            name = Console.ReadLine();
+            id = int.Parse(Console.ReadLine());
+        }
+        public void display()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Id: " + id);
+        }
+    }
+}
--- Chapter3/MultipleClass2/Program.cs
+++ Chapter3/MultipleClass2/Program.cs
@@ -2,21 +2,13 @@
 
 namespace MultipleClass2
 {
-    class Test
-    {
-        string name;
-        public void input()
-        {
-            name = "University";
-            Console.WriteLine(name);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Test obj = new Test();
+            Class1 obj = new Class1();
             obj.input();
+            obj.display();
         }
     }
 }
