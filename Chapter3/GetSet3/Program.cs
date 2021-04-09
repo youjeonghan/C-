a89c03b7e50f80e864b0c11203743d905f1e9231
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-09 17:34:24 +0900

    4주차-5 get, set
    get, set 사용

---
+++ Chapter3/GetSet3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GetSet3
+{
+    class Student
+    {
+        string name;
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj = new Student();
+            obj.Name = "Korea"; // set the value
+            Console.WriteLine("Name: " + obj.Name);
+        }
+    }
+}
