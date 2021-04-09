f437885009a95ec874bc23c8b7b9e257c07cabbd
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-09 13:05:42 +0900

    4주차-4
    생성자 사용

---
+++ Chapter3/Constructor1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Constructor1
+{
+    class Person
+    {
+        string name;
+        int id;
+        public Person(string name, int id)
+        {
+            this.name = name;   // 인자의 이름이 인스턴스 변수와 다르면 this. 사용안해도 가능 ex) string a -> name = a;
+            this.id = id;
+            Console.WriteLine(name+"\n"+id);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person obj = new Person("Michel", 2021);
+        }
+    }
+}
