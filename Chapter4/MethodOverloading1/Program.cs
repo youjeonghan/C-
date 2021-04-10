213b4bd4d96d860984eca2d7c3c1499251533deb
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-10 18:58:05 +0900

    5주차-3 overloading

---
+++ Chapter4/MethodOverloading1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodOverloading1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.input(7);
+            obj.input(10, 10.5);
+            obj.input(10, 10);
+        }
+        void input(int x)
+        {
+            Console.WriteLine("Output1: " + (x+x));
+        }
+        void input(int x, double y)
+        {
+            Console.WriteLine("Output2: " + (x + y));
+        }
+        void input(int x, int y)
+        {
+            Console.WriteLine("Output3: " + (x + y));
+        }
+    }
+}
---
+++ Chapter4/RandomNumber1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomNumber1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int face = rand.Next();
+            Console.WriteLine(face);
+        }
+    }
+}
