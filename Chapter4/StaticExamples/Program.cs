0ea066da91d9ad4b002cfaff6644f9b1dc6cd502
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-10 14:12:59 +0900

    5주차-1 static

---
+++ Chapter4/StaticExamples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticExamples
+{
+    class Program
+    {
+        static string name;
+        int age;
+        void input(string family, int age)  // non static
+        {
+            name = family;
+            this.age = age;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.input("Michel", 14);
+            Console.WriteLine(Program.name + "\n" + obj.age);
+        }
+    }
+}
