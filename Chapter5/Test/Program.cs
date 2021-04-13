3acf6014cdf3375c3662369c25bf93a20c8718fb
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-13 13:29:08 +0900

    6주차-1 array

---
+++ Chapter5/Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            for (int i=0; i<arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int[] arr1 = arr;
+            arr[0] = 100;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+        }
+    }
+}
