8c4b86dbe0b9d885ad806c225da2899cc2337b1a
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-14 19:30:58 +0900

    6주차 rolldie 코드

---
+++ Chapter5/RollDie/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RollDie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumbers = new Random();
+            var frequency = new int[7];
+            Console.WriteLine($"{"face",4}{"frequency",10}");
+            for (var face = 0; face < frequency.Length; ++face)
+            {
+                Console.WriteLine($"{face,4}{frequency[face],10}");
+            }
+
+
+            for (var roll = 1; roll<60000000; ++roll)
+            {
+                ++frequency[randomNumbers.Next(1, 7)];
+            }
+            Console.WriteLine($"{"face",4}{"frequency",10}");
+            for (var face = 0; face < frequency.Length; ++face)
+            {
+                Console.WriteLine($"{face,4}{frequency[face],10}");
+            }
+        }
+    }
+}
