a27f645daa513fed9a8fd639dd831821057b0102
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-05-16 20:36:20 +0900

    9주차-1

---
+++ Chapter6/LINQ_ListCollection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQ_ListCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<string>();
+            items.Add("aQua");
+            items.Add("Rust");
+            items.Add("yElow");
+            items.Add("rED");
+
+            Console.Write("items contains: ");
+            foreach (var item in items)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine();
+
+            var startsWithR =
+                from item in items
+                let uppercaseString = item.ToUpper()
+                where uppercaseString.StartsWith("R")
+                orderby uppercaseString
+                select uppercaseString;
+
+            Console.Write("results of query startsWithR : ");
+            foreach (var item in startsWithR)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
--- Chapter6/generic_class/Program.cs
+++ Chapter6/generic_class/Program.cs
@@ -12,7 +12,7 @@ namespace generic_class
             //display list's count and capacity before adding 
             Console.WriteLine("count before adding:" + items.Count);
             Console.WriteLine("Capacity before adding:" + items.Capacity);
-            items.Add("Red"); //append an item to the list 
+            items.Add("red"); //append an item to the list 
             items.Insert(0, "yellow"); //insert value at index 0 
             //display list's count and capacity after adding 
             Console.WriteLine("Count after adding:" + items.Count);
@@ -20,13 +20,34 @@ namespace generic_class
             
             //display using for loop 
             Console.Write("Display using for loop:");
+            for (var i = 0; i < items.Count; i++)
+            {
+                Console.Write(items[i] + " ");
+            }
+
+            //display using foreach loop 
+            Console.Write("\nDisplay using foreach loop:");
+            
             foreach (var i in items)
             {
                 Console.Write(i + " ");
-            } 
-            
-            //display using foreach loop 
-            Console.Write("\nDisplay using foreach loop:");
+            }
+
+            items.Add("green");
+            items.Add("yellow");
+
+            Console.WriteLine("\ncount before adding:" + items.Count);
+            Console.WriteLine("Capacity before adding:" + items.Capacity);
+
+            Console.Write("Display using for loop:");
+            for (var i = 0; i < items.Count; i++)
+            {
+                Console.Write(items[i] + " ");
+            }
+
+            items.Remove("yellow");//remove the first yellow
+
+            Console.Write("\nDisplay using for loop:");
             for (var i = 0; i < items.Count; i++)
             {
                 Console.Write(items[i] + " ");
