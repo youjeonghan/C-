958cec924cb1a6153fa07b204975b374506c2496
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-05-16 21:10:38 +0900

    9주차-2

---
+++ Chapter8/MultiLevel/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MultiLevel
+{
+    class Students
+    {
+        int id;
+        string name;
+        public void getInfo()
+        {
+            Console.WriteLine("Enter Student detail:");
+            name = Console.ReadLine();
+            id = int.Parse(Console.ReadLine());
+        }
+        public void putInfo()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(id);
+        }
+    }
+    class ComputerStu : Students
+    {
+        int m1, m2;
+        public void getmark()
+        {
+            Console.WriteLine("Enter two subject mark:");
+            m1 = int.Parse(Console.ReadLine());
+            m2 = int.Parse(Console.ReadLine());
+        }
+        public void putmark()
+        {
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            Console.WriteLine("Total:" + (m1 + m2));
+        }
+    }
+    class Display : ComputerStu
+    {
+        void display()
+        {
+            Console.WriteLine("****Output***");
+            putInfo();
+            putmark();
+        }
+        static void Main(string[] args)
+        {
+            Display obj = new Display();
+            obj.getInfo();
+            obj.getmark();
+            obj.display();
+        }
+    }
+}
---
+++ Chapter8/Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Test
+{
+    // sealed class A   // 이러면 상속못함
+    class A //parent class 
+    {
+        public void display1()
+        {
+            Console.WriteLine("Parent class display1");
+        }
+        public void display2()
+        {
+            Console.WriteLine("Parent class display2");
+        }
+    }
+    //child class 
+    class B : A //inheritance 
+    {
+        void display3()
+        {
+            Console.WriteLine("child class");
+        }
+        static void Main(string[] args)
+        {
+            B obj = new B();
+            obj.display1();
+            obj.display2();
+            obj.display3();
+        }
+    }
+
+}
