b0476da394aed97b77314623d4d56d91a91a7027
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-05-24 19:05:05 +0900

    11주차-1

---
+++ Chapter8/Iparent/Child.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iparent
+{
+    class Child : Iparent, Iparent1
+    {
+        public void sum(int a, int b) //implementation of interface 
+        {
+            Console.WriteLine("Add:" + (a + b));
+        }
+        public void sub(int a, int b) //implementation of interface 
+        {
+            Console.WriteLine("Sub:" + (a - b));
+        }
+        public void mul(int a, int b)
+        {
+            Console.WriteLine("Mul:" + (a * b));
+        }
+        static void Main(string[] args)
+        {
+            Child obj = new Child();
+            obj.sum(20, 30);
+            obj.sub(10, 30);
+            obj.mul(10, 20);
+        }
+    }
+}
---
+++ Chapter8/Iparent/Iparent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iparent
+{
+    interface Iparent
+    {
+        //by default method under interface are public 
+        //by defacult methods are abstract 
+        void sum(int a, int b);
+        void sub(int a, int b);
+    }
+    interface Iparent1
+    {
+        void mul(int a, int b);
+    }
+}
