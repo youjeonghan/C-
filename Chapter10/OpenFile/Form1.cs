a84d7764609f868cb99b4922c4933a04f64c296f
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-05-26 16:37:53 +0900

    12주차-2

---
+++ Chapter10/OpenFile/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //open file dialog to read file 
+            OpenFileDialog TestDialog = new OpenFileDialog();
+            // Show testDialog as a modal dialog and determine if DialogResult = OK.
+            if (TestDialog.ShowDialog() == DialogResult.OK)
+            { 
+                //load picture to picture box 
+                pictureBox1.ImageLocation = TestDialog.FileName;
+            }
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
