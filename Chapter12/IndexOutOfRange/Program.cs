df2c3e34c9eb1f415d78de9e64d552ac1b35c8e7
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-05-30 01:06:13 +0900

    13주차-3

---
+++ Chapter12/IndexOutOfRange/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IndexOutOfRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 10, 20, 30, 40, 50 };
+            try
+            {
+                //try to access the value outside the range 
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(arr[i]); //exception thrown 
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("this is finally");
+            }
+        }
+    }
+}
---
+++ Chapter12/Throw/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Throw
+{
+    class ExceptionExample
+    {
+        int a, b, result = 0;
+        public void display()
+        {
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                //throw the custom messsage from programmer 
+                if (b == 0) throw new Exception("Divide by zero"); //new exception is thrown 
+                result = a / b;
+                Console.WriteLine("Result:" + result);
+            }
+            catch (Exception e) //catch the explicit exception 
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionExample obj = new ExceptionExample();
+            obj.display();
+        }
+    }
+}
